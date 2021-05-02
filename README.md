cd55522d65e993c97f5c0f10a10dc84d1e9a532a
Mirza Akhena <mirza.akhena@gmail.com>
2021-05-02 11:34:36 +0700

    update test

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Retry
+
+
+There are 2 variable that trigger the retry mechanism
+- Max Retry
+  ErrorProducer < MaxRetry
+  ErrorProducer == MaxRetry
+  ErrorProducer > MaxRetry
+  
+- Error Type
+    Any
+    Specific and only one
+    Specific and multiple in first
+    Specific and multiple in middle
+    Specific and multiple in last
+
+Test:
+- ErrorProducer < MaxRetry, ErrorType is Any
+- ErrorProducer < MaxRetry, ErrorType is Specific and only one
+- ErrorProducer < MaxRetry, ErrorType is Specific and multiple in first
+- ErrorProducer < MaxRetry, ErrorType is Specific and multiple in middle
+- ErrorProducer < MaxRetry, ErrorType is Specific and multiple in last
--- retry_test.go
+++ retry_test.go
@@ -84,11 +84,11 @@ func TestErrorProducer_001(t *testing.T) {
 
 }
 
-// TestRetry_002 should run 3 times because it listen to any error
+// TestRetry_002 should run 3 times because of max retry and listen to any error
 func TestRetry_002(t *testing.T) {
 
-	// error will occur 3 times, success is in the 4th attempts
-	e := newErrorProducer(3, fmt.Errorf("direct error fmt"), "test")
+	// error will occur 5 times, success is in the 6th attempts
+	e := newErrorProducer(5, fmt.Errorf("direct error fmt"), "test")
 
 	r := New()
 	err := r.SetConfig(Config{
@@ -124,7 +124,26 @@ func TestRetry_003(t *testing.T) {
 	r.OnRetry(func() error { return e.Run() })
 }
 
-// TestRetry_003 retry should only error 3 times and in the fourth times, it will success (no need to retry)
+// TestRetry_0031 retry should run once since the error type does not match
+func TestRetry_0031(t *testing.T) {
+	e := newErrorProducer(3, fmt.Errorf("direct error fmt"), "test")
+
+	r := New()
+	err := r.SetConfig(Config{
+		MaxRetry: 3,
+		DelayFunc: func(attempt int) time.Duration {
+			return 1 * time.Second
+		},
+		ByErrors: []error{structErrorType{}}, // <--- only this type of error
+	})
+	assert.Nil(t, err)
+	r.SetRetryListener(func(info Info) {
+		fmt.Printf("%v %v\n", info.RetryCount, info.Error)
+	})
+	r.OnRetry(func() error { return e.Run() })
+}
+
+// TestRetry_004 retry should only error 3 times and in the fourth times, it will success (no need to retry)
 func TestRetry_004(t *testing.T) {
 	e := newErrorProducer(3, fmt.Errorf("direct error fmt"), "test")
 
@@ -135,6 +154,44 @@ func TestRetry_004(t *testing.T) {
 			return 1 * time.Second
 		},
 		ByErrors: DefaultAnyError(),
+	})
+	assert.Nil(t, err)
+	r.SetRetryListener(func(info Info) {
+		fmt.Printf("%v %v\n", info.RetryCount, info.Error)
+	})
+	r.OnRetry(func() error { return e.Run() })
+}
+
+// TestRetry_005 retry should only error 3 times and in the fourth times, it will success (no need to retry)
+func TestRetry_005(t *testing.T) {
+	e := newErrorProducer(3, structErrorType{}, "test")
+
+	r := New()
+	err := r.SetConfig(Config{
+		MaxRetry: 10, // <--- we set Max Retry to 10
+		DelayFunc: func(attempt int) time.Duration {
+			return 1 * time.Second
+		},
+		ByErrors: []error{structErrorType{}},
+	})
+	assert.Nil(t, err)
+	r.SetRetryListener(func(info Info) {
+		fmt.Printf("%v %v\n", info.RetryCount, info.Error)
+	})
+	r.OnRetry(func() error { return e.Run() })
+}
+
+// TestRetry_006 retry should only error 3 times and in the fourth times, it will success (no need to retry)
+func TestRetry_006(t *testing.T) {
+	e := newErrorProducer(3, compositionErrorType{}, "test")
+
+	r := New()
+	err := r.SetConfig(Config{
+		MaxRetry: 10, // <--- we set Max Retry to 10
+		DelayFunc: func(attempt int) time.Duration {
+			return 1 * time.Second
+		},
+		ByErrors: []error{stringErrorType(""), compositionErrorType{}},
 	})
 	assert.Nil(t, err)
 	r.SetRetryListener(func(info Info) {
